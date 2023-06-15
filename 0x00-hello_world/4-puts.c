c47b42211d4d85b4e4eb21d2f4faf1d646477046
Nimoh53 <faithnimoh53@gmail.com>
2023-06-15 11:49:39 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 # include <stdio.h>
 /**
- * main - A program used to output Programming is like building a multilingual puzzle
+ * main - A program that outputs statement using puts
  * Return: 0 (Success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return 0;
+return (0);
 }
