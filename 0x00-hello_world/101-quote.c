609e84939ab4e522ec26420633079f0f580de500
Nimoh53 <faithnimoh53@gmail.com>
2023-06-15 14:01:45 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - a program that prints a statement and outputs in standard error
@@ -6,6 +5,6 @@
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+ write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+    return 1;
 }
