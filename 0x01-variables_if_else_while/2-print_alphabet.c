e9dd189fac2f47b6b5084fd7198123da616f521c
Nimoh53 <faithnimoh53@gmail.com>
2023-06-16 14:34:18 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,17 +2,17 @@
 /**
  * main - Prints the alphabet.
  *
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 int i;
-for (i = 0; i < 26; i++)
+for (i = 0; i < 52; i++)
 {
 putchar(alp[i]);
 }
-putchar("\n");
+putchar('\n');
 return (0);
 }
 
