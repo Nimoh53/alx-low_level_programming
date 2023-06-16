2ebff24461b9d4c4eb634712ebe49aec6f7f2489
Nimoh53 <faithnimoh53@gmail.com>
2023-06-16 15:16:27 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
- int i;
- for (i = 48; i < 58; i++)
- {
- putchar(i);
- }
- for (i = 97; i < 103; i++)
+int i;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+for (i = 97; i < 103; i++)
 {
 putchar(i);
 }
