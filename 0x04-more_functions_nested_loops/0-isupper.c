e128546551cf7213b2914a7fc3ae43f2937c7874
Nimoh53 <faithnimoh53@gmail.com>
2023-06-22 21:06:30 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - Checks if a letter is supper
+ * @c: The number to be checked
+ * Return: 1 for upper letter and 0 for anything else
+ */
+int _isupper(int c)
+{
+if(x >= 65 && x <= 90)
+{
+return (1);
+}
+return (0):
+}
