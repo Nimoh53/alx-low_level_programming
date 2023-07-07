6334cc1d2a3a5cfe4afab45a19f66091f8cdd77d
Nimoh53 <faithnimoh53@gmail.com>
2023-07-07 12:09:03 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Alwats 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", *argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,4 @@
+#define MAIN_H
+#ifndef MAIN_H
+int main(int argc, char *argv[])
+#endif
