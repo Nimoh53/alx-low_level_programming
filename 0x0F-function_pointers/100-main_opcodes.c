11fd8f4d2726d22789fe98cfccfa930b4ed06ad4
Nimoh53 <faithnimoh53@gmail.com>
2023-07-19 14:53:22 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/** main - Prints the opcodes of itself.
+/** 
+ * main - Prints the opcodes of itself.
  * @argc: The number of arguments supplied to the program
  * @argv: An array of pointers to the arguments
  * Return: Always 0
  */
 int main(int argc, char *argv[])
-{       
+{
 int bytes, index;
 int (*address)(int, char **) = main;
 unsigned char opcode;
 if (argc != 2)
-{       
+{
 printf("Error\n");
 exit(1);
 }
 bytes = atoi(argv[1]);
 if (bytes < 0)
-{       
+{
 printf("Error\n");
 exit(2);
 }
