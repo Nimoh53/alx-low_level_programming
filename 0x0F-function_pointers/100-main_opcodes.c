7f053ab80af5ac4442246b8366fca543bcdd6889
Nimoh53 <faithnimoh53@gmail.com>
2023-07-19 15:02:11 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-/** 
+/**
  * main - Prints the opcodes of itself.
  * @argc: The number of arguments supplied to the program
  * @argv: An array of pointers to the arguments
